80a1f7b9790bdd327c259f123decc6d603a6fbed
Olivia Hill <oliviahill99992@gmail.com>
2022-05-31 11:56:47 +0100

    preview enabled

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { CanvasClient } from "@uniformdev/canvas";
+import {
+  CanvasClient,
+  CANVAS_DRAFT_STATE,
+  CANVAS_PUBLISHED_STATE,
+} from "@uniformdev/canvas";
 import { Composition, Slot } from "@uniformdev/canvas-react";
 import resolveRenderer from "../lib/resolveRenderer";
 
-export async function getStaticProps() {
+// LESSON 7 - ACTIVITY 6- START
+import { useLivePreviewNextStaticProps } from "../hooks/useLivePreviewNextStaticProps";
+import getConfig from "next/config";
+// LESSON 7 - ACTIVITY 6 - END
+
+export async function getStaticProps({ preview }) {
   const client = new CanvasClient({
     apiKey: process.env.UNIFORM_API_KEY,
     projectId: process.env.UNIFORM_PROJECT_ID,
   });
   const { composition } = await client.getCompositionBySlug({
     slug: "/",
+    state: preview ? CANVAS_DRAFT_STATE : CANVAS_PUBLISHED_STATE,
   });
   return {
     props: {
@@ -19,7 +29,18 @@ export async function getStaticProps() {
   };
 }
 
+// LESSON 7 - ACTIVITY 6 - START
+const { publicRuntimeConfig } = getConfig();
+const { uniform } = publicRuntimeConfig;
+// LESSON 7 - ACTIVITY 6 - END
+
 export default function Home({ composition }) {
+  // LESSON 7 - ACTIVITY 6 - START
+  useLivePreviewNextStaticProps({
+    compositionId: composition?._id,
+    projectId: uniform.projectId,
+  });
+  // LESSON 7 - ACTIVITY 6 - END
   return (
     <Composition data={composition} resolveRenderer={resolveRenderer}>
       <div className={styles.container}>
